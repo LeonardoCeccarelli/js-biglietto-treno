dbe41760f525cb0accb1b690749fdaac2aee30fd
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-10-08 14:54:55 +0200

    create html and js folder

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const prezzoAlKm = 0.21;
+let kmTotali = prompt("Quanti chilometri vorrebbe percorrere in treno? (Tariffa di 0,21 €/Km)")
+
+kmTotali = parseInt(kmTotali)
+
+// console.log(kmTotali)
+// console.log(typeof kmTotali)
+
