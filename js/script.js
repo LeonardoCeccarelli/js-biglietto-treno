9192834f653c5be7639bd72169317d4d5a378a9b
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-10-11 10:10:51 +0200

    indented inside else codes

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
 const prezzoAlKm = 0.21
 let kmTotali = prompt("Quanti chilometri vorrebbe percorrere in treno? (Tariffa di 0,21 €/Km)")
 
-kmTotali = parseInt(kmTotali)
+kmTotali = kmTotali.replace(",", ".")
+
+kmTotali = parseFloat(kmTotali)
 
 if (isNaN(kmTotali)) {
     alert("Attenzione: il numero inserito non è corretto")
+} else {
+    let eta = prompt("Inserire l'età")
+
+    eta = parseInt(eta)
+
+    if (isNaN(eta)) {
+        alert("Attenzione: il numero inserito non è corretto")
+    } else {
+        let prezzoBiglietto = kmTotali * prezzoAlKm
+
+        if (eta < 18) {
+            alert("La sua età rientra nella fascia dove le verrà applicato uno sconto pari al 20% al prezzo intero del biglietto.");
+            prezzoBiglietto = prezzoBiglietto - ((prezzoBiglietto * 20) / 100)
+        } else if (eta >= 65) {
+            alert("La sua età rientra nella fascia dove le verrà applicato uno sconto pari al 40% al prezzo intero del biglietto.");
+            prezzoBiglietto = prezzoBiglietto - ((prezzoBiglietto * 40) / 100)
+        }
+
+        prezzoBiglietto = prezzoBiglietto.toFixed(2)
+
+        alert("Il biglietto ha un costo di: " + prezzoBiglietto + "€.")
+    }
 }
 
-let eta = prompt("Inserire l'età")
 
-eta = parseInt(eta)
-
-if (isNaN(eta)) {
-    alert("Attenzione: il numero inserito non è corretto")
-}
-
-let prezzoBiglietto = kmTotali * prezzoAlKm
-
-console.log(prezzoBiglietto)
-
-if (eta < 18) {
-    alert("La sua età rientra nella fascia dove le verrà applicato uno sconto pari al 20% al prezzo intero del biglietto.");
-    prezzoBiglietto = prezzoBiglietto - ((prezzoBiglietto * 20) / 100)
-} else if (eta >= 65) {
-    alert("La sua età rientra nella fascia dove le verrà applicato uno sconto pari al 40% al prezzo intero del biglietto.");
-    prezzoBiglietto = prezzoBiglietto - ((prezzoBiglietto * 40) / 100)
-}
-
-prezzoBiglietto = parseFloat(prezzoBiglietto).toFixed(2)
-
-console.log(prezzoBiglietto)
-
-alert("Il biglietto ha un costo di: " + prezzoBiglietto + "€.")
 
 
